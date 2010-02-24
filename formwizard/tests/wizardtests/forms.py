0d2d4f89071b24c9cc87b9644a763ad9d6fc06d5
steph <steph@rdev.info>
2010-02-24 18:29:37 +0100

    Added tests for formsets.

--- formwizard/tests/wizardtests/forms.py
+++ formwizard/tests/wizardtests/forms.py
@@ -3,6 +3,7 @@ from formwizard.forms import FormWizard
 from django.http import HttpResponse
 from django.template import Template, Context
 from django.contrib.auth.models import User
+from django.forms.formsets import formset_factory
 
 class Page1(forms.Form):
     name = forms.CharField(max_length=100)
@@ -15,7 +16,9 @@ class Page2(forms.Form):
     
 class Page3(forms.Form):
     random_crap = forms.CharField(max_length=100)
-    
+
+Page4 = formset_factory(Page3, extra=2)
+
 class ContactWizard(FormWizard):
     def done(self, request, form_list):
         c = Context({'form_list': [x.cleaned_data for x in form_list], 'all_cleaned_data': self.get_all_cleaned_data()})
--- formwizard/tests/wizardtests/tests.py
+++ formwizard/tests/wizardtests/tests.py
@@ -16,6 +16,13 @@ class WizardTests(object):
         },
         {
             'form3-random_crap': 'blah blah',
+        },
+        {
+            'form4-INITIAL_FORMS': '0',
+            'form4-TOTAL_FORMS': '2',
+            'form4-MAX_NUM_FORMS': '0',
+            'form4-0-random_crap': 'blah blah',
+            'form4-1-random_crap': 'blah blah',
         }
     )
 
@@ -30,10 +37,10 @@ class WizardTests(object):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context['form_step'], 'form1')
         self.assertEqual(response.context['form_step0'], 0)
-        self.assertEqual(response.context['form_last_step'], 'form3')
+        self.assertEqual(response.context['form_last_step'], 'form4')
         self.assertEqual(response.context['form_prev_step'], None)
         self.assertEqual(response.context['form_next_step'], 'form2')
-        self.assertEqual(response.context['form_step_count'], 3)
+        self.assertEqual(response.context['form_step_count'], 4)
 
     def test_form_post_error(self):
         response = self.client.post(self.wizard_url)
@@ -84,8 +91,14 @@ class WizardTests(object):
         self.assertEqual(response.context['form_step'], 'form3')
 
         response = self.client.post(self.wizard_url, self.wizard_step_data[2])
+
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.context['form_list'], [{'name': u'Pony', 'thirsty': True, 'user': self.testuser}, {'address1': u'123 Main St', 'address2': u'Djangoland'}, {'random_crap': u'blah blah'}])
+        self.assertEqual(response.context['form_step'], 'form4')
+
+        response = self.client.post(self.wizard_url, self.wizard_step_data[3])
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(response.context['form_list'], [{'name': u'Pony', 'thirsty': True, 'user': self.testuser}, {'address1': u'123 Main St', 'address2': u'Djangoland'}, {'random_crap': u'blah blah'}, [{'random_crap': u'blah blah'}, {'random_crap': u'blah blah'}]])
 
     def test_cleaned_data(self):
         response = self.client.get(self.wizard_url)
@@ -96,7 +109,9 @@ class WizardTests(object):
         self.assertEqual(response.status_code, 200)
         response = self.client.post(self.wizard_url, self.wizard_step_data[2])
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.context['all_cleaned_data'], {'name': u'Pony', 'thirsty': True, 'user': self.testuser, 'address1': u'123 Main St', 'address2': u'Djangoland', 'random_crap': u'blah blah'})
+        response = self.client.post(self.wizard_url, self.wizard_step_data[3])
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['all_cleaned_data'], {'name': u'Pony', 'thirsty': True, 'user': self.testuser, 'address1': u'123 Main St', 'address2': u'Djangoland', 'random_crap': u'blah blah', 'formset-form4': [{'random_crap': u'blah blah'}, {'random_crap': u'blah blah'}]})
 
 class SessionWizardTests(WizardTests, TestCase):
     wizard_url = '/wiz_session/'
