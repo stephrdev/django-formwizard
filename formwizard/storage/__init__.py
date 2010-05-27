d577262bc989f781654c1d2579515ce0406b96be
Stephan Jaekel <steph@rdev.info>
2010-05-27 13:03:36 +0200

    better exceptions for missing modules and missing classes

--- formwizard/storage/__init__.py
+++ formwizard/storage/__init__.py
@@ -5,15 +5,21 @@ try:
 except ImportError:
     from django.utils.importlib import import_module
 
+class MissingStorageModuleException(ImproperlyConfigured):
+    pass
+
+class MissingStorageClassException(ImproperlyConfigured):
+    pass
+
 def get_storage(path, *args, **kwargs):
     i = path.rfind('.')
     module, attr = path[:i], path[i+1:]
     try:
         mod = import_module(module)
     except ImportError, e:
-        raise ImproperlyConfigured('Error loading storage %s: "%s"' % (module, e))
+        raise MissingStorageModuleException('Error loading storage %s: "%s"' % (module, e))
     try:
         storage_class = getattr(mod, attr)
     except AttributeError:
-        raise ImproperlyConfigured('Module "%s" does not define a storage named "%s"' % (module, attr))
+        raise MissingStorageClassException('Module "%s" does not define a storage named "%s"' % (module, attr))
     return storage_class(*args, **kwargs)
