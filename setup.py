84be9bc77c0946b5dfba741977251acb9840e06c
Stephan Jaekel <steph@rdev.info>
2010-08-21 16:49:27 +0200

    updated setup to deploy on pypi.

--- setup.py
+++ setup.py
@@ -2,14 +2,17 @@ from setuptools import setup, find_packages
  
 setup(
     name='django-formwizard',
-    version='0.1',
+    version='0.4',
     description='A FormWizard for Django with multiple storage backends',
     author='Stephan Jaekel',
     author_email='steph@rdev.info',
     url='http://github.com/stephrdev/django-formwizard/',
-    packages=find_packages(),
+    packages=find_packages(exclude=['test_project', 'test_project.*']),
+    package_data = {
+        'formwizard': ['templates/*/*.html'],
+    },
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
