8f620212c6efd2138aaaa308381f09bf5b7e1c4b
Stephan Jaekel <steph@rdev.info>
2010-09-07 15:36:02 -0700

    added tests for file_support.

--- formwizard/tests/wizardtests/forms.py
+++ formwizard/tests/wizardtests/forms.py
@@ -4,6 +4,11 @@ from django.http import HttpResponse
 from django.template import Template, Context
 from django.contrib.auth.models import User
 from django.forms.formsets import formset_factory
+from django.core.files.storage import FileSystemStorage
+import tempfile
+
+temp_storage_location = tempfile.mkdtemp()
+temp_storage = FileSystemStorage(location=temp_storage_location)
 
 class Page1(forms.Form):
     name = forms.CharField(max_length=100)
@@ -13,13 +18,16 @@ class Page1(forms.Form):
 class Page2(forms.Form):
     address1 = forms.CharField(max_length=100)
     address2 = forms.CharField(max_length=100)
-    
+    file1 = forms.FileField()
+
 class Page3(forms.Form):
     random_crap = forms.CharField(max_length=100)
 
 Page4 = formset_factory(Page3, extra=2)
 
 class ContactWizard(FormWizard):
+    file_storage = temp_storage
+
     def done(self, request, storage, form_list, **kwargs):
         c = Context({'form_list': [x.cleaned_data for x in form_list], 'all_cleaned_data': self.get_all_cleaned_data(request, storage)})
         for form in self.form_list.keys():
