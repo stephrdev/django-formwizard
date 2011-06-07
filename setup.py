78e1ae21c60221668165aab12bc5fd4a9d4b641e
Stephan Jaekel <steph@rdev.info>
2011-06-07 12:28:06 +0200

    Updated version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-formwizard',
-    version='0.6',
+    version='1.0',
     description='A FormWizard for Django with multiple storage backends',
     author='Stephan Jaekel',
     author_email='steph@rdev.info',
